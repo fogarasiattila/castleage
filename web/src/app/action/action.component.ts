390ece43a91329f264a1eb4dc7d4bb4903f4c784
Attila Fogarasi <fogarasi.attila@ekretazrt.onmicrosoft.com>
2022-04-07 08:52:04 +0200

    action comp

--- web/src/app/action/action.component.ts
+++ web/src/app/action/action.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Group } from 'src/interfaces/group';
+import { Player } from 'src/interfaces/player';
+import { PlayerService } from '../services/player.service';
 
 @Component({
   selector: 'app-action',
   templateUrl: './action.component.html',
-  styleUrls: ['./action.component.css']
+  styleUrls: ['./action.component.css'],
 })
 export class ActionComponent implements OnInit {
+  groups: Group[] = [];
+  players: Player[] = [];
+  selectedGroups: Group[] = [];
+  selectedPlayers: Player[] = [];
 
-  constructor() { }
+  constructor(private playerService: PlayerService) {}
 
   ngOnInit(): void {
+    this.playerService.groupsState$.subscribe({
+      next: (groups) => {
+        this.groups = groups;
+        this.selectedGroups = groups;
+      },
+    });
+    this.playerService.playersState$.subscribe({
+      next: (players) => {
+        this.players = players;
+        this.selectedPlayers = players;
+      },
+    });
   }
 
+  onGroupSelected() {}
+  onPlayerSelected() {}
 }
--- web/src/app/action/action.module.ts
+++ web/src/app/action/action.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActionComponent } from './action.component';
 import { ActionRoutingModule } from './action-routing.module';
+import { PlayerModule } from '../player/player.module';
+import { MaterialModule } from '../material/material.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ActionComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, PlayerModule, MaterialModule, FormsModule],
   exports: [ActionRoutingModule],
 })
 export class ActionModule {}
